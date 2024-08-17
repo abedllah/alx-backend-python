101d3eab1ce7d5fe1e6e4b6698a09cbfad253b73
Abdellah <abdellahtaghouni@gmail.com>
2024-08-16 17:25:34 -0700

    task4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""
-Define variables
-"""
+'''
+type-annotated function
+'''
 
 
 a: int = 1
 pi: float = 3.14
-i_understand_annotations: bool = true
-school: str = 'Holberton'
+i_understand_annotations: bool = True
+school: str = "Holberton"
